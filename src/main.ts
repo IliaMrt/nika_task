4145af773ff461fa09eaa17a09e9d179158cfb96
ilmar <ilmar1@protonmail.com>
2024-06-14 21:10:56 +0300

    настроен swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Тестовое задание компании Ника')
+    .setDescription('Описание допустимых запросов от клиента')
+    .setVersion('0.1.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -2,19 +2,26 @@ import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { RequestsService } from "./requests.service";
 import { RequestDto } from "./dto/request.dto";
 import { IdDto } from "./dto/id.dto";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller('requests')
+@ApiTags('API клиентских заявок')
 export class RequestsController {
   constructor(private requestsService: RequestsService) {}
   @Post()
+  @ApiOperation({ summary: 'Создание заявки' })
   async createNewRequest(@Body() request: RequestDto) {
     return await this.requestsService.createNewRequest(request);
   }
   @Get(':id')
+  @ApiOperation({ summary: 'Получение заявки по id' })
+
   async getRequest(@Param() id: IdDto) {
     return await this.requestsService.getRequest(id);
   }
   @Put(':id')
+  @ApiOperation({ summary: 'Редактирование заявки по id' })
+
   async updateRequest(@Param() id: IdDto, @Body() request: RequestDto) {
     return await this.requestsService.updateRequest(id, request);
   }
